bfb3d379f6329c923c25695c97e5e36a0e5fce57
Alfonsus_Antero_A <renditobloggerspot@gmail.com>
2023-10-17 09:29:35 +0700

    Modul3

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useRef, useState } from "react";
+import { View, DrawerLayoutAndroid, StatusBar } from "react-native";
+import Header from "./components/header";
+import Button from "./components/button";
+import Separator from "./components/separator";
+import List from "./screens/list";
+import Article from "./screens/article";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+const App = () => {
+  const [page, setPage] = useState("list");
+  const drawer = useRef(null);
+
+  const changePage = (drawer, pageName) => {
+    drawer.current.closeDrawer();
+    setPage(pageName);
+  };
+
+  const navigationView = () => (
+    <View style={{ padding: 30, backgroundColor: "#222222", flex: 1 }}>
+      <Button text="List" onPress={() => changePage(drawer, "list")} />
+      <Separator height={30} />
+      <Button text="Article" onPress={() => changePage(drawer, "article")} />
+      <Separator height={30} />
+      <Button text="Close" onPress={() => drawer.current.closeDrawer()} />
     </View>
   );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return (
+    <DrawerLayoutAndroid
+      ref={drawer}
+      drawerWidth={300}
+      drawerPosition="left"
+      renderNavigationView={navigationView}
+    >
+      <StatusBar style="light" backgroundColor="#AA0002" />
+      <View>
+        <Header drawer={drawer} />
+        {page == "list" ? <List /> : page == "article" ? <Article /> : null}
+      </View>
+    </DrawerLayoutAndroid>
+  );
+};
+
+export default App;
