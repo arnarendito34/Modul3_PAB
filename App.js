aa9ca0dfa8a64d5124da2ce23ef73686c1519096
Alfonsus_Antero_A <renditobloggerspot@gmail.com>
2023-10-24 12:43:16 +0700

    update

--- App.js
+++ App.js
@@ -1,27 +1,35 @@
 import { useRef, useState } from "react";
 import { View, DrawerLayoutAndroid, StatusBar } from "react-native";
 import Header from "./components/header";
-import Button from "./components/button";
+import GButton from "./components/button";
 import Separator from "./components/separator";
 import List from "./screens/list";
 import Article from "./screens/article";
 
+// Functional Component
 const App = () => {
+  // State Declaration
   const [page, setPage] = useState("list");
+  // Ref Declaration
   const drawer = useRef(null);
 
+  // Arrow Function inside Functional Component
   const changePage = (drawer, pageName) => {
+    // Close Drawer
     drawer.current.closeDrawer();
+    // Change state value
+
     setPage(pageName);
   };
 
+  // Arrow Function inside Functional Component
   const navigationView = () => (
     <View style={{ padding: 30, backgroundColor: "#222222", flex: 1 }}>
-      <Button text="List" onPress={() => changePage(drawer, "list")} />
+      <GButton text="List" onPress={() => changePage(drawer, "list")} />
       <Separator height={30} />
-      <Button text="Article" onPress={() => changePage(drawer, "article")} />
+      <GButton text="Article" onPress={() => changePage(drawer, "article")} />
       <Separator height={30} />
-      <Button text="Close" onPress={() => drawer.current.closeDrawer()} />
+      <GButton text="Close" onPress={() => drawer.current.closeDrawer()} />
     </View>
   );
 
@@ -36,7 +44,7 @@ const App = () => {
       <View>
         <Header drawer={drawer} />
         {page == "list" ? <List /> : page == "article" ? <Article /> : null}
-      </View>
+      </View>      
     </DrawerLayoutAndroid>
   );
 };
--- components/separator.js
+++ components/separator.js
@@ -1,4 +1,4 @@
-import { View } from "react-native";
+import { View } from "@gluestack-ui/themed";
 
 const Separator = (props) => {
   return <View style={{ height: props.height }}></View>;
